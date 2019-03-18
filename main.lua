733378a1fa36bf43a64f1347b99ef26ffa807bb9
Mazhar Ahmed <mazhar@seazoobd.com>
2019-03-18 14:35:06 +0600

    made the debug message dynamic with screen size now random numbers are animated on screen

--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ function love.load()
         screens[i] = {}
         for j = 0, screen_height / char_height, 1
         do
-            screens[i][j] = 1
+            screens[i][j] = math.random(9)
         end
         
     end
@@ -29,7 +29,21 @@ end
 -- called everytime it needs to update frame
 function love.update(dt)
     -- update the time the game is running
+    -- old_running_time = running_time
     running_time = running_time + dt
+
+    -- now make random chars
+    -- if math.floor(old_running_time) < math.floor(running_time) then
+        for i = 0, screen_width / char_width, 1
+        do
+            screens[i] = {}
+            for j = 0, screen_height / char_height, 1
+            do
+                screens[i][j] = math.random(9)
+            end
+            
+        end
+    -- end
 end
 
 -- draw wherever I want here
@@ -49,9 +63,23 @@ function love.draw()
     -- draw the FPS
     if DEBUG then
         love.graphics.setColor(0, 0, 0, 1)
-        love.graphics.rectangle("fill", 695, 555, 500, 500 )
+        love.graphics.rectangle(
+            "fill",
+            screen_width - 105,
+            screen_height - 45,
+            500,
+            500
+        )
         love.graphics.setColor(0, 255, 0, 1)
-        love.graphics.print('Current FPS: ' .. tostring(love.timer.getFPS()), 700, 580)
-        love.graphics.print('Runtime: ' .. string.format("%0.1f", running_time) .. 's', 700, 560)
+        love.graphics.print(
+            'Current FPS: ' .. tostring(love.timer.getFPS()),
+            screen_width - 100,
+            screen_height - 20
+        )
+        love.graphics.print(
+            'Runtime: ' .. string.format("%0.1f", running_time) .. 's',
+            screen_width - 100,
+            screen_height - 40
+        )
     end
 end
