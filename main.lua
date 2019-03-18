d8e525aaa640d96233423630e55f95110f3c7d97
Mazhar Ahmed <mazhar@seazoobd.com>
2019-03-18 14:25:33 +0600

    added runtime / running time to the screen

--- main.lua
+++ main.lua
@@ -3,13 +3,17 @@ DEBUG = true
 
 -- called once the game opened
 function love.load()
+    -- running time is 0s at the beginning
+    running_time = 0
     -- first make an empty screen of chars
     screens = {}
     -- inspect the viewport and init the constants
     screen_width = love.graphics.getWidth()
     screen_height = love.graphics.getHeight()
+    -- size of a digit
     char_width = 10
     char_height = 12
+
     -- now make random chars
     for i = 0, screen_width / char_width, 1
     do
@@ -24,7 +28,8 @@ end
 
 -- called everytime it needs to update frame
 function love.update(dt)
-    -- TODO
+    -- update the time the game is running
+    running_time = running_time + dt
 end
 
 -- draw wherever I want here
@@ -44,8 +49,9 @@ function love.draw()
     -- draw the FPS
     if DEBUG then
         love.graphics.setColor(0, 0, 0, 1)
-        love.graphics.rectangle("fill", 695, 575, 500, 500 )
+        love.graphics.rectangle("fill", 695, 555, 500, 500 )
         love.graphics.setColor(0, 255, 0, 1)
-        love.graphics.print("Current FPS: "..tostring(love.timer.getFPS()), 700, 580)
+        love.graphics.print('Current FPS: ' .. tostring(love.timer.getFPS()), 700, 580)
+        love.graphics.print('Runtime: ' .. string.format("%0.1f", running_time) .. 's', 700, 560)
     end
 end
