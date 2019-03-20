d5eac2b1499d2355f160fcf68b891504ec7b525c
Mazhar Ahmed <mazhar@seazoobd.com>
2019-03-20 13:06:37 +0600

    randomized the digit animation, and made the digit animation based on initial time given to that digit

--- anim.lua
+++ anim.lua
@@ -9,7 +9,8 @@ local function anim()
             env['primary_color'][2],
             env['primary_color'][3],
             env['primary_color'][4]
-        }
+        },
+        times = 0
     }
 
     -- gets the char at d time after dt fraction
@@ -18,6 +19,9 @@ local function anim()
         -- if d % env['delay'] < 0.1 then
         --     return false
         -- end
+
+        -- increase the time
+        self.times = self.times + 1
 
         -- if the opacity hits 0, reset the digit
         if self.x[5] <= 0 or self.x[5] == nil then
@@ -39,7 +43,9 @@ local function anim()
 
         -- alter the secondary color with primary
         -- comparing numbers makes it speedy
-        if self.x[2] == env['secondary_color'][1] and d % env['secondary_delay'] < 0.01 then
+        -- TODO keep the white digit longer like 0.2 ~ 0.5s
+        if self.x[2] == env['secondary_color'][1] then
+            --d % env['secondary_delay'] < 0.01 then
             self.x[2] = env['primary_color'][1]
             self.x[3] = env['primary_color'][2]
             self.x[4] = env['primary_color'][3]
@@ -49,7 +55,8 @@ local function anim()
 
         -- just decrease the opacity and return it
         if self.x[2] ~= env['secondary_color'][1] then
-            self.x[5] = self.x[5] - 0.005
+            -- minus the dt to make the animation smoother
+            self.x[5] = self.x[5] - dt
         end
         
         return self.x
@@ -61,6 +68,16 @@ local function anim()
 
     function self.get_digit()
         return self.x[1]
+    end
+
+    function self.init(d)
+        if self.times == 0 then
+            for i = 0, math.floor(d / env['delay']), 1
+            do
+                -- reach that time period
+                self.get(d, env['delay'])
+            end
+        end
     end
 
     -- return the instance
--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 anim = require("anim")
+env = require("env").get()
 
 -- debug mode shows the FPS
 DEBUG = true
@@ -7,6 +8,8 @@ DEBUG = true
 function love.load()
     -- running time is 0s at the beginning
     running_time = 0
+    -- keep the frames as 0
+    total_frames = 0
     -- first make an empty screen of chars
     screens = {}
     -- inspect the viewport and init the constants
@@ -24,7 +27,7 @@ function love.load()
         do
             -- add a new anim obj
             screens[i][j] = anim()
-            -- screens[i][j].get(0.2, 1)
+            screens[i][j].init(math.random(20) * env['delay'], env['delay'])
         end
         
     end
@@ -35,6 +38,8 @@ function love.update(dt)
     -- update the time the game is running
     -- old_running_time = running_time
     running_time = running_time + dt
+    -- update the frame count
+    total_frames = total_frames + 1
 
     -- now make random chars
     -- if math.floor(old_running_time) < math.floor(running_time) then
@@ -68,21 +73,29 @@ function love.draw()
         love.graphics.setColor(0, 0, 0, 1)
         love.graphics.rectangle(
             "fill",
-            screen_width - 105,
-            screen_height - 45,
+            screen_width - 115,
+            screen_height - 65,
             500, -- just keep it larger than the message
             500  -- just keep it larger than the message too
         )
         love.graphics.setColor(0, 255, 0, 1)
+        -- print the framerate
         love.graphics.print(
             'Current FPS: ' .. tostring(love.timer.getFPS()),
-            screen_width - 100,
+            screen_width - 110,
             screen_height - 20
         )
+        -- print the total time in second
         love.graphics.print(
             'Runtime: ' .. string.format("%0.1f", running_time) .. 's',
-            screen_width - 100,
+            screen_width - 110,
             screen_height - 40
+        )
+        -- print the total frames rendered
+        love.graphics.print(
+            'Frames: ' .. tostring(total_frames),
+            screen_width - 110,
+            screen_height - 60
         )
     end
 end
